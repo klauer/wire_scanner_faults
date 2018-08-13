e343f15c12b680da4b3f4fb8f5323d3baca9fa1f
K Lauer <kenneth.lauer@gmail.com>
2018-08-13 09:46:58 -0700

    Change average current calculation; skip partial travel range results

--- commutation_search/plot_fws_calibration.py
+++ commutation_search/plot_fws_calibration.py
@@ -7,32 +7,50 @@ matplotlib.use('Qt5Agg')  # noqa
 import matplotlib.pyplot as plt
 
 
-def plot(data):
+def plot(data, skip):
     info = dict(offset=[], average=[], positions=[], current=[])
 
     for offset, (idx, pos_cmd, pos_fbk, cur_cmd, cur_fbk) in data.items():
         if offset < 180:
             offset += 360
 
-        pos_fbk = np.asarray(pos_fbk[30:-50]) / 1000.
-        cur_fbk = np.asarray(cur_fbk[30:-50])
-        avg = np.average(cur_fbk)
+        pos_fbk = np.asarray(pos_fbk) / 1000.
+        valid_idx = pos_fbk > 2.0
+        cur_fbk = np.asarray(cur_fbk)[valid_idx]
+        pos_fbk = pos_fbk[valid_idx]
+        max_pos = np.max(pos_fbk)
+
+        avg = np.average(cur_fbk[(pos_fbk >= 5) & (pos_fbk <= 41)])
+
+        if max_pos < 41.1 and skip:
+            print('Discarding offset {} (max pos = {})'
+                  ''.format(offset, max_pos))
+            pos_fbk = []
+            cur_fbk = []
+        else:
+            print('Including offset {} (max pos = {})'
+                  ''.format(offset, max_pos))
 
         info['offset'].append(offset)
         info['average'].append(avg)
         info['positions'].append(pos_fbk)
         info['current'].append(cur_fbk)
 
-    min_avg = np.argmin(info['average'])
-    min_offset = info['offset'][min_avg]
-    min_avg = info['average'][min_avg]
+    min_idx = np.argmin(info['average'])
+    min_offset = info['offset'][min_idx]
+    min_avg = info['average'][min_idx]
 
-    for offset, pos_fbk, cur_fbk in zip(info['offset'], info['positions'],
-                                        info['current']):
+    marker_positions = []
+
+    for offset, pos_fbk, cur_fbk, avg in zip(info['offset'], info['positions'],
+                                             info['current'], info['average']):
+        if not len(pos_fbk):
+            continue
+
         marker = ('x-' if offset == min_offset
                   else '--')
 
-        avg = np.average(cur_fbk)
+        marker_positions.append((offset, avg))
         alpha = (1.0 - (avg - min_avg) / avg) ** 3
         plt.plot(pos_fbk, cur_fbk, marker,
                  label='{} deg; {:.3f}A'.format(offset, avg),
@@ -54,16 +72,31 @@ def plot(data):
     plt.xlabel('Commutation offset [deg]')
     plt.ylabel('Average current over scan [A]')
     plt.plot('offset', 'average', data=info)
+    for offset, avg in marker_positions:
+        plt.plot([offset], [avg], 'x')
     plt.show()
     return info
 
 
+def _parse_args():
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description='Calibrate FWS commutation offset'
+    )
+    parser.add_argument(
+        'filenames', type=str, nargs='+',
+        help='List of dataset filenames (e.g., results-300-330.txt)')
+    parser.add_argument(
+        '--no-skip', action='store_true', default=False,
+        help='Include results which do not allow for the full travel range')
+    return parser.parse_args(sys.argv[1:])
+
+
 if __name__ == '__main__':
-    try:
-        fns = sys.argv[1:]
-    except IndexError:
-        print('Usage: {} results_filename.txt'.format(sys.argv[0]))
-        sys.exit(1)
+    args = _parse_args()
+    fns = args.filenames
+    skip = not args.no_skip
 
     full_data = {}
     for fn in fns:
@@ -73,4 +106,4 @@ if __name__ == '__main__':
         full_data.update(ast.literal_eval(data))
 
     # plt.ion()
-    info = plot(full_data)
+    info = plot(full_data, skip=skip)
